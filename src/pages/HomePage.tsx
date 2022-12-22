4189f036c0e2f33ad59713035b9032c012b0bb66
yuksak <gulamaier@gmail.com>
2022-12-22 22:10:20 +0500

    refactor: import seacher and cards list

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { CardsList, Searcher } from "components";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="container mx-auto">
+      <Searcher />
+      <CardsList />
+    </div>
+  );
 };
