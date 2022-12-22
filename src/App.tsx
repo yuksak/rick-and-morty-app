7f9bd8ce32791a4f3e20d4ebae7cf7f2a74d92d1
yuksak <gulamaier@gmail.com>
2022-12-22 17:04:00 +0500

    feat: add pages into routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components";
+import { AboutPage, HomePage, NotFoundPage } from "./pages";
+
+const routes = [
+  { path: "/", element: <HomePage /> },
+  { path: "/about", element: <AboutPage /> },
+  { path: "*", element: <NotFoundPage /> },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <p>Rick and Morty</p>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        {routes.map((route) => (
+          <Route path={route.path} element={route.element} />
+        ))}
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
