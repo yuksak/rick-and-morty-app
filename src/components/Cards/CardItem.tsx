a927eee1e3e91c489ce44570647d0d43b00e184d
yuksak <gulamaier@gmail.com>
2022-12-24 15:50:38 +0500

    refactor: change arrow right icon

--- src/components/Cards/CardItem.tsx
+++ src/components/Cards/CardItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HiArrowRight } from "react-icons/hi";
 
 export const CardItem = () => {
   return (
@@ -21,15 +22,7 @@ export const CardItem = () => {
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-gradient-to-br from-cyan-500 to-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-zinc-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Read more
-          <svg
-            aria-hidden="true"
-            className="w-4 h-4 ml-2 -mr-1"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"></path>
-          </svg>
+          <HiArrowRight className="w-4 h-4 ml-2 -mr-1" />
         </a>
       </div>
     </div>
