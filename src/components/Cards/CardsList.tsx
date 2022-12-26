cdea9d6fa5a534cd3ebd4fc1bcac5626d0ba181b
yuksak <gulamaier@gmail.com>
2022-12-27 02:25:09 +0500

    feat: add an item component and map results

--- src/components/Cards/CardsList.tsx
+++ src/components/Cards/CardsList.tsx
@@ -1,12 +1,16 @@
-import React from "react";
-import { CardItem } from "./CardItem";
-import { CardItemSkeleton } from "./CardItemSkeleton";
+import React from 'react';
+import { useAppSelector } from 'hooks/redux-hooks';
+
+import { CardItem } from './CardItem';
 
 export const CardsList = () => {
+  const { results } = useAppSelector((state) => state.characters);
+
   return (
     <div className="grid justify-center sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 grid-flow-row gap-4 mb-5 p-2 sm:p-0">
-      <CardItem />
-      <CardItemSkeleton />
+      {results.map((result) => (
+        <CardItem {...result} key={result.id} />
+      ))}
     </div>
   );
 };
