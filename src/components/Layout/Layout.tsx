07079240020fa5d26e46fe432fb06a5cdbc7d969
yuksak <gulamaier@gmail.com>
2022-12-26 23:47:35 +0500

    refactor: add darkMode class into layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router-dom";
-import { Footer, Navbar } from "../index";
+import { useAppSelector } from 'hooks/redux-hooks';
+import { Outlet } from 'react-router-dom';
+import { Footer, Navbar } from '../index';
 
 export const Layout = () => {
+  const { darkMode } = useAppSelector((state) => state.darkMode);
+
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="my-1 py-8" />
-      <Navbar />
-      <Outlet />
-      <Footer />
+    <div className={`${darkMode ? 'dark' : ''}`}>
+      <div className="min-h-screen flex flex-col dark:bg-zinc-900">
+        <div className="my-1 py-8" />
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
     </div>
   );
 };
