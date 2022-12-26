3d00126a1cae77fb9cf42db6e61842dd78caf0ce
yuksak <gulamaier@gmail.com>
2022-12-26 23:41:43 +0500

    feat: dispatch all data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { CardsList, Searcher } from "components";
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from 'hooks/redux-hooks';
+
+import { getAllCharacters } from 'api';
+import { CardsList, Searcher, SpinnerAlert } from 'components';
 
 export const HomePage = () => {
+  const { status } = useAppSelector((state) => state.characters);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getAllCharacters());
+  }, []);
+
   return (
     <div className="container mx-auto">
       <Searcher />
-      <CardsList />
+      {status === 'Fulfilled' ? <CardsList /> : <SpinnerAlert />}
     </div>
   );
 };
