515fc20c1849d8275bdcef95407692fff4ac30e6
yuksak <gulamaier@gmail.com>
2022-12-27 02:24:18 +0500

    refactor: fix and update style

--- src/components/Cards/CardItem.tsx
+++ src/components/Cards/CardItem.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { HiArrowRight } from "react-icons/hi";
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
-export const CardItem = () => {
+import { HiArrowRight } from 'react-icons/hi';
+import { ICharacter } from 'store/slices/characterSlice';
+
+export const CardItem: FC<ICharacter> = (props) => {
+  const id = props.id.toString();
+
   return (
-    <div className="max-w-sm bg-white border border-zinc-200 rounded-lg dark:bg-zinc-800 dark:border-zinc-700">
-      <a href="#">
-        <img
-          className="rounded-t-lg"
-          src="https://picsum.photos/500/290"
-          alt="image"
-        />
-      </a>
-      <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-xl font-bold tracking-tight text-zinc-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-        </a>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-gradient-to-br from-cyan-500 to-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-zinc-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    <div className="relative rounded-lg border overflow-hidden bg-white border-secondary-lightest dark:bg-secondary-darker dark:border-secondary">
+      <Link to={id} className="block w-full relative h-48">
+        <img className="w-96 h-full object-cover" src={props.image} alt="image" />
+        <div className="w-full absolute top-0 h-full object-cover bg-gradient-to-t from-secondary-darkest" />
+      </Link>
+      <div className="p-5 absolute items-center bottom-0 bg-gradient-to- from-secondary-darkest w-full transition-all ease-in duration-100 ">
+        <Link to={id}>
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-white">{props.name}</h5>
+        </Link>
+        <Link
+          to={id}
+          className="inline-flex items-center px-2 py-1 text-xs font-medium text-center rounded-lg text-white bg-primary hover:bg-gradient-to-br from-primary-light to-primary focus:outline-none dark:bg-secondary"
         >
           Read more
-          <HiArrowRight className="w-4 h-4 ml-2 -mr-1" />
-        </a>
+          <HiArrowRight className="w-3 h-3 ml-1" />
+        </Link>
       </div>
     </div>
   );
