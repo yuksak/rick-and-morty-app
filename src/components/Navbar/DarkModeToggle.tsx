dd3dc86169e73360da4fc2dd0fbe99afc5b45f1d
yuksak <gulamaier@gmail.com>
2022-12-26 23:48:38 +0500

    feat: implement dark mode enabler

--- src/components/Navbar/DarkModeToggle.tsx
+++ src/components/Navbar/DarkModeToggle.tsx
@@ -1,22 +1,30 @@
-import React, { FC } from "react";
-import { MdLightMode, MdDarkMode } from "react-icons/md";
+import React from 'react';
+import { useAppDispatch, useAppSelector } from 'hooks/redux-hooks';
 
-interface IDarkModeToggleProps {
-  darkTheme: boolean;
-  onClick: (val: (val: boolean) => boolean) => void;
-}
+import { setDarkMode } from 'store/slices';
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
 
-export const DarkModeToggle: FC<IDarkModeToggleProps> = ({
-  onClick,
-  darkTheme,
-}) => {
+export const DarkModeToggle = () => {
+  const { darkMode } = useAppSelector((state) => state.darkMode);
+  const dispatch = useAppDispatch();
+
+  const toggleHandler = () => {
+    if (!darkMode) {
+      localStorage.setItem('darkMode', 'enabled');
+      dispatch(setDarkMode(true));
+    } else {
+      localStorage.removeItem('darkMode');
+      dispatch(setDarkMode(false));
+    }
+  };
+
   return (
     <button
       type="button"
       className="text-zinc-500 dark:text-yellow-400 text-xl cursor-pointer md:border border-zinc-300 rounded-lg p-3 focus:ring-blue-500 dark:border-zinc-600"
-      onClick={() => onClick((theme: boolean) => !theme)}
+      onClick={toggleHandler}
     >
-      {darkTheme ? <MdDarkMode /> : <MdLightMode />}
+      {darkMode ? <MdDarkMode /> : <MdLightMode />}
     </button>
   );
 };
