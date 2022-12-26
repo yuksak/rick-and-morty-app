c11418b0facfeea8ae81ab1c8cd0e5d9119d2e50
yuksak <gulamaier@gmail.com>
2022-12-27 02:13:58 +0500

    refactor: change color class names

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const NotFoundPage = () => {
   return (
-    <div className="container mx-auto p-2 text-zinc-900 dark:text-white">
+    <div className="container mx-auto p-2 text-secondary-darkest dark:text-white">
       <h2 className="text-5xl font-bold bold py-2">
         Don't know <br />
         where you are?
@@ -13,7 +13,7 @@ export const NotFoundPage = () => {
       <Link
         to="/"
         type="button"
-        className="uppercase text-white bg-gradient-to-br from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        className="uppercase text-white bg-gradient-to-br from-cyan-500 to-primary hover:bg-gradient-to-bl focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
       >
         Back to home page
       </Link>
