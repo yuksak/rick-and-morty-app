87eeafed8f6f88376b6f588887be953d1670de07
yuksak <gulamaier@gmail.com>
2022-12-26 23:57:54 +0500

    refactor: change urls

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,33 +1,26 @@
-import React from "react";
+import React from 'react';
 
-import { FiGithub } from "react-icons/fi";
-import { TfiLinkedin } from "react-icons/tfi";
+import { FiGithub } from 'react-icons/fi';
+import { TfiLinkedin } from 'react-icons/tfi';
 
 const links = [
-  { name: "github", path: "https://github.com/yuksak", logo: <FiGithub /> },
+  { name: 'github', path: 'https://github.com/yuksak', logo: <FiGithub /> },
   {
-    name: "linkedin",
-    path: "https://linkedin.com/in/yuksak",
+    name: 'linkedin',
+    path: 'https://linkedin.com/in/yuksak',
     logo: <TfiLinkedin />,
   },
 ];
 
 export const Footer = () => {
   return (
-    <footer className="p-4 mx-2 mb-2 mt-auto sm:mb-5 border-zinc-200 dark:border-zinc-600 bg-white rounded-lg border border-g md:flex md:items-center md:justify-between md:p-6 dark:bg-zinc-800">
+    <footer className="p-4 mx-2 mb-2 mt-auto sm:mb-5 border-zinc-200 dark:border-zinc-600 bg-white rounded-lg border border-g flex flex-col items-center sm:flex-row sm:justify-between md:p-6 dark:bg-zinc-800">
       <span className="text-sm text-zinc-500 sm:text-center dark:text-zinc-400">
-        © 2022{" "}
-        <a href="https://github.com/yuksak" className="hover:underline">
-          Yuksak
-        </a>
-        . All Rights Reserved.
+        © 2022 Yuksak. All Rights Reserved.
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-xl text-zinc-500 dark:text-zinc-400 sm:mt-0 gap-3">
         {links.map((link) => (
-          <li
-            key={link.name}
-            className="flex hover:text-blue-500 items-center hover:animate-pulse"
-          >
+          <li key={link.name} className="flex hover:text-blue-500 items-center hover:animate-pulse">
             <a href={link.path}>{link.logo}</a>
           </li>
         ))}
