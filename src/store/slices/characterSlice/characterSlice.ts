035e48bee251bb171823bfac2627f35f4fb19550
yuksak <gulamaier@gmail.com>
2022-12-29 20:15:40 +0500

    feat: connect searchCharacters with state

--- src/store/slices/characterSlice/characterSlice.ts
+++ src/store/slices/characterSlice/characterSlice.ts
@@ -1,38 +1,67 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { getAllCharacters } from "api/characters";
+import { createSlice } from '@reduxjs/toolkit';
+import { getCharacters, searchCharacters } from 'api/characters/characters';
+import { urlValueGetter } from 'utils';
 
-import { ICharacterSlice } from "./characterSlice.types";
+import { ICharacterSlice } from './characterSlice.types';
 
 const initialState: ICharacterSlice = {
-  info: { count: 0, pages: 0, next: "", prev: "" },
+  info: { count: 0, pages: 0, next: 0, prev: 0 },
   results: [],
-  status: "Pending",
+  status: 'Pending',
 };
 
 const characterSlice = createSlice({
-  name: "cards",
+  name: 'cards',
   initialState,
   reducers: {},
 
   extraReducers: (builder) => {
     builder
-      .addCase(getAllCharacters.pending, (state) => {
+      .addCase(getCharacters.pending, (state) => {
         state.results = [];
-        state.status = "Pending";
-        state.info = { count: 0, pages: 0, next: "", prev: "" };
+        state.status = 'Pending';
+        state.info = { count: 0, pages: 0, next: 0, prev: 0 };
       })
 
-      .addCase(getAllCharacters.fulfilled, (state, { payload }) => {
-        state.results = payload.results;
-        state.info = payload.info;
-        state.status = "Fulfilled";
+      .addCase(getCharacters.fulfilled, (state, { payload: { info, results } }) => {
+        const newInfo = {
+          ...info,
+          prev: urlValueGetter(info.prev, 'page'),
+          next: urlValueGetter(info.next, 'page'),
+        };
+
+        state.results = results;
+        state.info = newInfo;
+        state.status = 'Fulfilled';
       })
 
-      .addCase(getAllCharacters.rejected, (state) => {
-        state.status = "Rejected";
+      .addCase(getCharacters.rejected, (state) => {
+        state.status = 'Rejected';
+      });
+
+    builder
+      .addCase(searchCharacters.pending, (state) => {
+        state.results = [];
+        state.status = 'Pending';
+        state.info = { count: 0, pages: 0, next: 0, prev: 0 };
+      })
+
+      .addCase(searchCharacters.fulfilled, (state, { payload: { info, results } }) => {
+        const newInfo = {
+          ...info,
+          prev: urlValueGetter(info.prev, 'page'),
+          next: urlValueGetter(info.next, 'page'),
+        };
+
+        state.results = results;
+        state.info = newInfo;
+        state.status = 'Fulfilled';
+      })
+
+      .addCase(searchCharacters.rejected, (state) => {
+        state.status = 'Rejected';
       });
   },
 });
 
-// export const { sortCards, prevPage, setPage, nextPage } = cardsSlice.actions;
 export const characterReducer = characterSlice.reducer;
