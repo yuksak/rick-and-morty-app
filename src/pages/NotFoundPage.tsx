24e31ff834ef09e3f725c2a599432ac9275430dd
yuksak <gulamaier@gmail.com>
2022-12-29 20:28:34 +0500

    refactor: add button to back to home

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'components';
+import { HiHome } from 'react-icons/hi';
 
 export const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="container mx-auto p-2 text-secondary-darkest dark:text-white">
       <h2 className="text-5xl font-bold bold py-2">
@@ -10,13 +13,10 @@ export const NotFoundPage = () => {
         where you are?
       </h2>
       <p className="mb-5">We really have no idea either...</p>
-      <Link
-        to="/"
-        type="button"
-        className="uppercase text-white bg-gradient-to-br from-cyan-500 to-primary hover:bg-gradient-to-bl focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-      >
+      <Button onClick={() => navigate('/')} classes="uppercase">
+        <HiHome className="mr-2" />
         Back to home page
-      </Link>
+      </Button>
     </div>
   );
 };
