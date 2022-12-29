535e8c7a2b77a9989ddcfd95cde299b8958fed29
yuksak <gulamaier@gmail.com>
2022-12-29 19:59:58 +0500

    refactor: add reusable button

--- src/components/Cards/CardItem.tsx
+++ src/components/Cards/CardItem.tsx
@@ -1,29 +1,28 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { HiArrowRight } from 'react-icons/hi';
 import { ICharacter } from 'store/slices/characterSlice';
+import { Button } from 'components/Button';
 
 export const CardItem: FC<ICharacter> = (props) => {
   const id = props.id.toString();
+  const navigate = useNavigate();
 
   return (
     <div className="relative rounded-lg border overflow-hidden bg-white border-secondary-lightest dark:bg-secondary-darker dark:border-secondary">
       <Link to={id} className="block w-full relative h-48">
         <img className="w-96 h-full object-cover" src={props.image} alt="image" />
-        <div className="w-full absolute top-0 h-full object-cover bg-gradient-to-t from-secondary-darkest" />
+        <div className="w-full absolute top-0 h-full object-cover bg-gradient-to-r from-secondary-darkest opacity-80" />
       </Link>
       <div className="p-5 absolute items-center bottom-0 bg-gradient-to- from-secondary-darkest w-full transition-all ease-in duration-100 ">
         <Link to={id}>
           <h5 className="mb-2 text-xl font-bold tracking-tight text-white">{props.name}</h5>
         </Link>
-        <Link
-          to={id}
-          className="inline-flex items-center px-2 py-1 text-xs font-medium text-center rounded-lg text-white bg-primary hover:bg-gradient-to-br from-primary-light to-primary focus:outline-none dark:bg-secondary"
-        >
+        <Button onClick={() => navigate(id)} classes="text-xs opacity-90 text-white border">
           Read more
           <HiArrowRight className="w-3 h-3 ml-1" />
-        </Link>
+        </Button>
       </div>
     </div>
   );
