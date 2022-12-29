9aca98214bee96ca46e011e29ce04c08d0467bb0
yuksak <gulamaier@gmail.com>
2022-12-29 20:13:48 +0500

    feat: implement search functionality

--- src/components/Searcher/Searcher.tsx
+++ src/components/Searcher/Searcher.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from 'hooks/redux-hooks';
+import { CharacterQuery, searchCharacters } from 'api';
+
 import { FiSearch } from 'react-icons/fi';
+import { Button } from 'components/Button';
 
 export const Searcher = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const input = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const value = input.current?.value;
+
+    dispatch(
+      searchCharacters({
+        query: CharacterQuery.name,
+        queryValue: value ? value : '',
+      })
+    );
+    navigate(value ? `/?${CharacterQuery.name}=${value}` : '');
+  };
+
   return (
     <form className="my-5 max-w-sm mx-2 sm:mx-0 relative">
       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -10,16 +33,17 @@ export const Searcher = () => {
       <input
         type="search"
         id="default-search"
-        className="block outline-none rounded-lg w-full p-2 sm:p-4 pl-10 sm:pl-10 text-sm text-secondary-darkest border border-secondary-lightest round focus:border-primary dark:bg-secondary-darker dark:border-secondary dark:placeholder-secondary-lighter dark:text-white dark:focus:border-primary"
+        className="block outline-none rounded-lg w-full p-2 sm:p-4 pl-10 sm:pl-10 text-sm text-secondary-darkest border border-secondary-lightest round focus:border-primary dark:bg-secondary-dark dark:border-secondary dark:placeholder-secondary-lighter dark:text-white dark:focus:border-primary"
         placeholder="Search"
+        ref={input}
         required
       />
-      <button
-        type="submit"
-        className="transition-all ease-in duration-75 text-white absolute right-1 sm:right-2 bottom-1.5 sm:bottom-2.5 px-2 py-1 sm:px-4 sm:py-2 focus:outline-none font-medium rounded-md sm:rounded-lg text-sm bg-primary hover:hover:bg-gradient-to-br dark:bg-secondary dark:hover:bg-gradient-to-br from-cyan-500 to-primary"
+      <Button
+        onClick={submitHandler}
+        classes={'absolute right-1 sm:right-2 bottom-1 sm:bottom-2.5'}
       >
         Search
-      </button>
+      </Button>
     </form>
   );
 };
