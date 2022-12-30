ecf781adb653ba41c4e180d469eb4b6918be998f
yuksak <gulamaier@gmail.com>
2022-12-30 18:12:53 +0500

    fix: url of link component

--- src/components/Cards/CardItem.tsx
+++ src/components/Cards/CardItem.tsx
@@ -11,15 +11,18 @@ export const CardItem: FC<ICharacter> = (props) => {
 
   return (
     <div className="relative rounded-lg border overflow-hidden bg-white border-secondary-lightest dark:bg-secondary-darker dark:border-secondary">
-      <Link to={id} className="block w-full relative h-48">
+      <Link to={`/character/${id}`} className="block w-full relative h-48">
         <img className="w-96 h-full object-cover" src={props.image} alt="image" />
         <div className="w-full absolute top-0 h-full object-cover bg-gradient-to-r from-secondary-darkest opacity-80" />
       </Link>
       <div className="p-5 absolute items-center bottom-0 bg-gradient-to- from-secondary-darkest w-full transition-all ease-in duration-100 ">
-        <Link to={id}>
+        <Link to={`/character/${id}`}>
           <h5 className="mb-2 text-xl font-bold tracking-tight text-white">{props.name}</h5>
         </Link>
-        <Button onClick={() => navigate(id)} classes="text-xs opacity-90 text-white border">
+        <Button
+          onClick={() => navigate(`/character/${id}`)}
+          classes="text-xs opacity-90 text-white border"
+        >
           Read more
           <HiArrowRight className="w-3 h-3 ml-1" />
         </Button>
