a5eab81635c2f6b189ac04e1d5bad35aab116224
yuksak <gulamaier@gmail.com>
2022-12-30 20:38:31 +0500

    refactor: change style to make more ux

--- src/components/Lists/ListItem.tsx
+++ src/components/Lists/ListItem.tsx
@@ -8,8 +8,10 @@ interface IListItemProps {
 export const ListItem: FC<IListItemProps> = ({ title, info }) => {
   return (
     <div className="flex flex-col pt-3">
-      <dt className="mb-1 text-secondary-light md:text-lg dark:text-secondary-lighter">{title}</dt>
-      <dd className="text-lg font-semibold">{info}</dd>
+      <dt className="mb-1 text-secondary-light text-xs md:text-lg dark:text-secondary-lighter">
+        {title}
+      </dt>
+      <dd className="text-sm md:text-lg font-semibold">{info}</dd>
     </div>
   );
 };
--- src/pages/ReadMorePage/ReadMorePage.tsx
+++ src/pages/ReadMorePage/ReadMorePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { useAppSelector } from 'hooks/redux-hooks';
 import { Button, Lists } from 'components';
+import { HiArrowLeft } from 'react-icons/hi';
 
 export const ReadMorePage = () => {
   const navigation = useNavigate();
@@ -20,7 +21,8 @@ export const ReadMorePage = () => {
 
   return (
     <div className="container mx-auto p-2 mb-10">
-      <Button classes="my-5" onClick={() => navigation('/')}>
+      <Button classes="mb-4 mt-0 md:my-5 text-sm sm:text-md" onClick={() => navigation('/')}>
+        <HiArrowLeft className="w-3 h-3 mr-1" />
         Go back
       </Button>
       <Lists {...data} />
