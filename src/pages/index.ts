a4568de0e4969c2707b10c324ca1ae589fa0e8d4
yuksak <gulamaier@gmail.com>
2022-12-30 18:17:50 +0500

    refactor: update exports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
-export * from "./HomePage";
-export * from "./AboutPage";
-export * from "./NotFoundPage";
+export * from './HomePage';
+export * from './AboutPage';
+export * from './NotFoundPage';
+export * from './ReadMorePage';
