2423dbb199bfa19ae3e659a345909bcf1a9acb2b
yuksak <gulamaier@gmail.com>
2022-12-30 20:27:16 +0500

    feat: add filter component to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useAppSelector } from 'hooks/redux-hooks';
-import { CardsList, Pagination, Searcher, SpinnerAlert } from 'components';
+import { CardsList, Filter, Pagination, Searcher, SpinnerAlert } from 'components';
 
 export const HomePage = () => {
   const { status, info } = useAppSelector((state) => state.characters);
 
   return (
-    <div className="container mx-auto">
-      <Searcher />
+    <div className="container mx-auto p-2 md:px-0">
+      <div className="flex flex-row gap-3 justify-between mb-3 mt-0 sm:mt-1">
+        <Filter />
+        <Searcher />
+      </div>
+
       {status === 'Fulfilled' ? (
         <>
           <CardsList />
