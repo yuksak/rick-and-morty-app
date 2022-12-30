4d92bec686dcdf725389fbc481bf3c75c67f5d9c
yuksak <gulamaier@gmail.com>
2022-12-30 20:24:20 +0500

    reafctor: change style classes

--- src/components/Cards/CardsList.tsx
+++ src/components/Cards/CardsList.tsx
@@ -7,7 +7,7 @@ export const CardsList = () => {
   const { results } = useAppSelector((state) => state.characters);
 
   return (
-    <div className="grid justify-center sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 grid-flow-row gap-4 mb-5 p-2 sm:p-0">
+    <div className="grid justify-center sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 grid-flow-row gap-3 sm:gap-4 mb-5">
       {results.map((result) => (
         <CardItem {...result} key={result.id} />
       ))}
