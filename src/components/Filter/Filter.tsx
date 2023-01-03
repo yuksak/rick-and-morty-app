8c805f336d93203b3318270c604321bde15a374b
yuksak <gulamaier@gmail.com>
2023-01-03 16:10:43 +0500

    refactor: change values of filter's option

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -4,7 +4,7 @@ import { useAppDispatch } from 'hooks/redux-hooks';
 import { CharacterQuery, searchCharacters } from 'api';
 
 const options = [
-  { value: '', name: 'Sort by status', selected: true },
+  { value: 'sort', name: 'Sort by status', selected: true },
   { value: '', name: 'All', selected: false },
   { value: 'Alive', name: 'Alive', selected: false },
   { value: 'Dead', name: 'Dead', selected: false },
@@ -24,11 +24,12 @@ export const Filter = () => {
   return (
     <div className="w-48">
       <select
+        defaultValue="sort"
         onChange={selectHandler}
         className="bg-gray-50 border h-full border-gray-300 text-secondary-darkest text-sm rounded-lg focus:border-primary-light block w-full p-2.5 dark:bg-secondary-darker dark:border-secondary dark:placeholder-secondary-light dark:text-white outline-none cursor-pointer dark:focus:border-primary-light"
       >
         {options.map(({ selected, name, value }) => (
-          <option selected={selected} value={value} disabled={selected}>
+          <option key={name} value={value} disabled={selected}>
             {name}
           </option>
         ))}
